f3d2c3ca3c64b5d49398d0e22cff1c310e27c951
Ricky Ayoub <vgmoose@gmail.com>
2015-06-10 00:07:17 -0400

    add directory changing support to shell plugin
    Signed-off-by: Ricky Ayoub <vgmoose@gmail.com>

--- plugins/shell.py
+++ plugins/shell.py
@@ -4,7 +4,7 @@ class Responder(baseresponder.BaseResponder):
 	
 	def __init__(self):
 		super(Responder, self).__init__()
-		self.curdir = os.path.expanduser("~")
+		self.curdir = "cd " + os.path.expanduser("~")
 	
 	def process(self, args, data):
 		super(Responder, self).process(args, data)
@@ -15,7 +15,16 @@ class Responder(baseresponder.BaseResponder):
 		# if a command is specified
 		if "cmd" in data:
 			cmd = data["cmd"]	
-			
+		
+		# update the path if this is a cd command
+		if cmd.startswith("cd "):
+			self.curdir = cmd
+			return ""
+		
+		# update the command with the target directory
+		cmd = self.curdir + "; " + cmd
+		
+					
 		# for debug mode just print the command, don't run it
 		if "debug" in data:
 			return cmd
